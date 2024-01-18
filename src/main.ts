c81a6eca4fcad2636ad4ea299fa3291ed43c06d5
Thomas Gomes <thomasgomes_cpti@outlook.com>
2024-01-18 18:44:53 -0300

    init project

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import Aurelia from 'aurelia';
+import { MyApp } from './app/my-app';
+
+Aurelia
+  .app(MyApp)
+  .start();
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'backgroud-home': '#875346',
+        'font-geral': '#4a1d1f'
+      }
+    },
+  },
+  plugins: [],
+}
